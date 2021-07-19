72f8d529cffc919876e4fa37afc2e676b5065b29
Dario <dario.defrancisci92@gmail.com>
2021-07-19 13:03:22 +0200

    caricato scaffolding

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//Scrivi un programma che stampi i numeri da 1 a 100.
+
+//Per i multipli di 3 stampi “Fizz” al posto del numero.
+
+//Per i multipli di 5 stampi "Buzz"al posto del numero.
+
+//Per i multipli sia di 3 che di 5 stampi "FizzBuzz".
