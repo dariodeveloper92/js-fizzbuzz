141cf1b51f3599e05f7b92e7f8f59015d5e09d78
Dario <dario.defrancisci92@gmail.com>
2021-07-19 14:23:37 +0200

    Esercizio Terminato

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-//Scrivi un programma che stampi i numeri da 1 a 100.
+//Scrivi un programma che stampi i numeri da 1 a 100.----------------------------------------------------
 
-//Per i multipli di 3 stampi “Fizz” al posto del numero.
+var numInterazioni = 100;
 
-//Per i multipli di 5 stampi "Buzz"al posto del numero.
-
-//Per i multipli sia di 3 che di 5 stampi "FizzBuzz".
+for (i = 1; i <= numInterazioni; i++) {
+    if ((i % 3 == 0) && (i % 5 == 0)) {//Per i multipli sia di 3 che di 5 stampi "FizzBuzz".
+        console.log('FizzBuzz')
+    }   else if (i % 3 == 0) {//Per i multipli di 3 stampi “Fizz” al posto del numero.
+            console.log('Fizz')
+    }   else if (i % 5 == 0) {//Per i multipli di 5 stampi "Buzz"al posto del numero.
+        console.log('Buzz')
+    }   else {
+        console.log(i + 1)
+    }
+}
